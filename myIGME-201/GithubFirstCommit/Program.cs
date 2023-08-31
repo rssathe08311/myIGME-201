9c27c3a9cb3e00d16d5dae8c7d353228c5a949d9
reva <reva@REVA23EF>
2023-08-30 22:35:08 -0400

    commiting to github??

---
+++ myIGME-201/GithubFirstCommit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubFirstCommit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+        }
+    }
+}
