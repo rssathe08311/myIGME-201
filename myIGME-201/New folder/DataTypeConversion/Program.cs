0ca52cc3e0ed6df8e42138c66c3b58cce56846bb
reva <reva@REVA23EF>
2023-09-05 22:56:29 -0400

    Question 5 working solution

---
+++ myIGME-201/New folder/DataTypeConversion/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double doubleNum = 9999.999; //fastest data type to use in calculations
+            float floatNum = 999.9F;
+            decimal decimalNum = 1234.5678M;
+            long longInt = -12345678;   // Int64 (64-bit)
+            ulong ulongInt = 12345678;  // UInt64
+            int intNum = -786;          // Int32
+            uint uintNum = 786;         // UInt32
+            short shortInt = -789;      // Int16
+            ushort ushortInt = 456;     // UInt16
+            byte byteNum = 254;         // 8-bit unsigned
+            sbyte sbyteNum = -123;      // 8-bit signed
+
+            longInt = shortInt;
+            uintNum = byteNum;
+
+            //byteNum = shortInt; 
+            //cannot put a larger data type in a smaller data type
+
+            //explicit casting
+            byteNum = (byte)shortInt;
+
+            try
+            {
+                byteNum = checked((byte)shortInt);
+                byteNum = Convert.ToByte(shortInt);
+            }
+            catch
+            {
+                //output message that data will be lost
+            }
+           
+        }
+    }
+}
---
+++ myIGME-201/Sathe_PE3/PE3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string to hold the user input from the console
+            string conInput = null;
+            //int that will be printed once altered 
+            int prod = 1;
+           
+            //for loop that gathers, converts, and then multiplies user input to generate the final product
+            for(int i = 0; i < 4; i++)
+            {
+                //prompts and gathers user input
+                Console.WriteLine("Enter an Integer: ");
+                conInput = Console.ReadLine();
+
+                //multiplies the user input once converted to the product
+                prod *= Convert.ToInt32(conInput);
+            }
+
+            //prints the final product to the console
+            Console.WriteLine(prod);
+        }
+    }
+}
