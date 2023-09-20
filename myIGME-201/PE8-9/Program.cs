cc83b06d19594d20c8bc3fe8348f1f5535d594ba
reva <reva@REVA23EF>
2023-09-19 21:10:32 -0400

    Working PE8 Question 9

---
+++ myIGME-201/PE8-9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+
+            string ans = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = "\"" + words[i] + "\"";
+               
+                ans += words[i] + " ";
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
