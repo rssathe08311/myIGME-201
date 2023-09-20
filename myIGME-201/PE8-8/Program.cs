f0f2b25ea9e4bbe7dcfb730d14bf75b831fc3029
reva <reva@REVA23EF>
2023-09-19 21:06:57 -0400

    Completed PE8 question 8

---
+++ myIGME-201/PE8-8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence that contains the word 'no':");
+            string sentence = Console.ReadLine();
+
+            sentence = sentence.ToLower();
+
+            string[] words = sentence.Split(' ');
+
+            string ans = "";
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == "no")
+                {
+                    words[i] = "yes";
+                }
+
+                ans += words[i] + " ";
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
