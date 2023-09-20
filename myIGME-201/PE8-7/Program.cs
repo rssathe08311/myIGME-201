c268ebacfdc00f0713aecef92134248a4f50c476
reva <reva@REVA23EF>
2023-09-19 20:53:07 -0400

    Completed Question 7

---
+++ myIGME-201/PE8-7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word: ");
+            string word = Console.ReadLine();
+
+            char[] chars = word.ToCharArray();
+
+            word = "";
+
+            for (int i = chars.Length -1 ; i >= 0; i--) 
+            {
+                word += chars[i];
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
