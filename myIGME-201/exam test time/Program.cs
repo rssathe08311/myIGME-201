781784a386a7b91bbdb4768d3cf04f2a1a008585
reva <reva@REVA23EF>
2023-10-01 18:08:19 -0400

    Word sorting program completed

---
+++ myIGME-201/exam test time/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_test_time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte byteVal;
+            short shortVal = -556;
+            byteVal = Convert.ToByte(shortVal);
+            Console.WriteLine("byteVal = {0}", byteVal);
+
+        }
+    }
+}
