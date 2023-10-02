ae3d6c5b518a97a6524103e10819a0ce7182e8da
reva <reva@REVA23EF>
2023-10-01 20:01:22 -0400

    Working Question 12

---
+++ myIGME-201/Question12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //local variables
+            string sName;
+            double dSalary = 30000;
+            bool gotSal;
+
+            Console.WriteLine("What's your name? ");
+            sName = Console.ReadLine();
+
+            gotSal = GiveRaise(sName, ref dSalary);
+
+            if (gotSal)
+            {
+                Console.WriteLine("Congratulations on the raise! Your salary is now: $" + dSalary);
+            }
+        }
+
+        static bool GiveRaise(string name, ref double salary)
+        {
+            bool gotSal = false;
+            name = name.ToLower();
+            if(name == "reva")
+            {
+                salary += 19999.99;
+                gotSal = true;
+            }
+
+            return gotSal;
+        }
+
+    }
+}
---
+++ myIGME-201/Question8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question8
+{
+    //defines the delegate type and data type
+    delegate double MathRound(double num1, int num2);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creates an instance of the delegate
+            MathRound mathRound;
+
+            //variables for calculation
+            double z = 0.0;
+            int xInd = 0;
+            int yInd = 0;
+
+            //calls method that completes the function of the delegate
+            mathRound = new MathRound(roundNum);
+
+            double[,,] ans = new double[21, 31, 3];
+
+            for (double x = 0; x <= 4; x += 0.1, xInd++)
+            {
+                yInd = 0;
+                for (double y = -1; y <= 1; y += 0.1, yInd++)
+                {
+                    //z = 4y3 + 2x2 - 8x + 7
+                    z = (4 * Math.Pow(y, 3)) + (2 * Math.Pow(x, 2)) - (8 * x) + 7;
+
+                    ans[xInd, yInd, 0] = x;
+                    ans[xInd, yInd, 1] = y;
+                    ans[xInd, yInd, 2] = z;
+
+                }
+            }
+        }
+
+        //method that performs the function of the delegate
+        static double roundNum(double num1, int num2)
+        {
+            return Math.Round(num1, num2);
+        }
+    }
+}
