f60f9720ceac9191720e48a28ebe58288acf612a
reva <reva@REVA23EF>
2023-10-10 16:21:54 -0400

    Finished PE11 Question 6

---
+++ myIGME-201/Traffic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void AddPassenger(IPassengerCarrier carrier) 
+        {
+            carrier.LoadPassenger();
+            if(carrier.GetType()  == typeof(Vehicle)) 
+            {
+                Console.WriteLine(carrier.ToString());
+            }
+        }
+    }
+}
