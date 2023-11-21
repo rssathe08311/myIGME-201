c071ade3365ebe7259b0a8229c078993fd39310b
reva <reva@REVA23EF>
2023-11-20 21:57:57 -0500

    Finished TriviaApp

---
+++ myIGME-201/TriviaApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace TriviaApp
+{
+    class TriviaResult
+    {
+        public string category;
+        public string type;
+        public string difficulty;
+        public string question;
+        public string correct_answer;
+        public List<string> incorrect_answers;
+    }
+
+    class Trivia
+    {
+        public int response_code;
+        public List<TriviaResult> results;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = null;
+            string s = null;
+
+            HttpWebRequest request;
+            HttpWebResponse response;
+            StreamReader reader;
+
+            url = "https://opentdb.com/api.php?amount=1&type=multiple";
+
+            request = (HttpWebRequest)WebRequest.Create(url);
+            response = (HttpWebResponse)request.GetResponse();
+            reader = new StreamReader(response.GetResponseStream());
+            s = reader.ReadToEnd();
+            reader.Close();
+
+            Trivia trivia = JsonConvert.DeserializeObject<Trivia>(s);
+
+            trivia.results[0].question = HttpUtility.HtmlDecode(trivia.results[0].question);
+            trivia.results[0].correct_answer = HttpUtility.HtmlDecode(trivia.results[0].correct_answer);
+
+            for (int i = 0; i < trivia.results[0].incorrect_answers.Count; ++i)
+            {
+                trivia.results[0].incorrect_answers[i] = HttpUtility.HtmlDecode(trivia.results[0].incorrect_answers[i]);
+            }
+        }
+    }
+}
